a9bed585954ea383512aeba5a56572598f1a501e
ARDAlion1104 <142300127+ARDAlion1104@users.noreply.github.com>
2023-08-22 17:11:05 +0300

    Update StarvingArtists.lua

--- StarvingArtists.lua
+++ StarvingArtists.lua
@@ -297,9 +297,9 @@ import(image)
 ArtMakerTab:AddButton({
 	Name = "Anime",
 	Callback = function()
-      		local image = 'https://www.seekpng.com/png/detail/423-4236056_cute-pixel-art-bunny.png' -- image you want to import
-local resolutionX = 45 -- usually it's 32 but it might change depending on the frame?
-local resolutionY = 45 -- usually it's 32 but it might change depending on the frame?
+      		local image = 'https://upload-os-bbs.hoyolab.com/upload/2022/11/05/8409043/90988373648173e6b97fa040a1297202_910222633013648728.png?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_80' -- image you want to import
+local resolutionX = 32 -- usually it's 32 but it might change depending on the frame?
+local resolutionY = 32 -- usually it's 32 but it might change depending on the frame?
 -- epic coding stuf --
 local grid = nil
 local s, e = pcall(function()
